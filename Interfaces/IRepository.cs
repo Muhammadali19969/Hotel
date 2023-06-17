e0486ec441d9bc4ec2dab09ac670fd1a2204f24c
MrX Rustamkulov <muhammadalirustamkulov160@gmail.com>
2023-06-17 17:57:26 +0500

    Add new Method to Utils and add new Method to Interfeces folder

--- Interfaces/IRepository.cs
+++ Interfaces/IRepository.cs
@@ -1,6 +1,20 @@
 ﻿
+using Hotel.Utils;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace Hotel.Interfaces;
 
-public interface IRepository
+public interface IRepository<TEntity, TViewModel>
 {
+    public Task<int> CreateAsync(TEntity obj);
+
+    public Task<int> UpdateAsync(long id , TEntity obj);
+
+    public Task<int> DeleteAsync(long id);
+
+    public Task<IList<TViewModel>> GetAllAsync(PagenationParams @params);
+
+    public Task<IList<TViewModel>> GetAllAsync(long id);
+
 }
